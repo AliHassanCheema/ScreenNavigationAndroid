bf8ebdd5a8928222f16092c3a99287d6a67cae4f
Mahmoodmehsud <mehmudmehsud101@gmail.com>
2022-08-11 16:03:24 +0500

    working on form

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,15 +2,27 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import kotlin.math.log
 
 class SecondActivity : AppCompatActivity() {
-    lateinit var secText : TextView
+    private lateinit var secText : TextView
+    private lateinit var onSave : Button
+    private lateinit var emailAddress : EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         secText = findViewById(R.id.secondText)
-        secText.text = "ALi Hassan"
+        onSave = findViewById(R.id.saveBtn)
+        emailAddress = findViewById(R.id.emailAddress)
+        secText.text = ""
+
+        onSave.setOnClickListener {
+            if(emailAddress.text.isEmpty() ) secText.text = "Text Field cant be empty" else secText.text = emailAddress.text
+        }
 
     }
 }
