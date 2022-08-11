08e1c0c2155d9f40d9855e5e07e3a60249eb4464
Mahmoodmehsud <mehmudmehsud101@gmail.com>
2022-08-11 14:14:03 +0500

    .

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -2,10 +2,14 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SecondActivity : AppCompatActivity() {
+    lateinit var secText : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        secText = findViewById(R.id.secondText)
+        secText.text = "ALi Hassan"
     }
 }
