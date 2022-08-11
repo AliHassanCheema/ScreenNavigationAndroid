4226e19fdcdfb8272b2e177c9ce6746e71e53931
Mahmoodmehsud <mehmudmehsud101@gmail.com>
2022-08-11 12:11:44 +0500

    project added to github

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+     lateinit var naviBtn : Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        naviBtn = findViewById(R.id.navigationBtn)
+        naviBtn.setOnClickListener{
+            startActivity(Intent(this, SecondActivity ::class.java))
+
+
+        }
+    }
+}
