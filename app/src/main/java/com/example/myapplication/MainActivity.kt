101abc02dec8041508a14445a924cce5cbdd7816
Ali Hassan <ahcheema12@gmail.com>
2022-08-12 10:19:41 +0500

    data passing from one activity to another

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-     lateinit var naviBtn : Button
+     private lateinit var naviBtn : Button
+     private lateinit var userName: TextView
+     private lateinit var email: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         naviBtn = findViewById(R.id.navigationBtn)
+        userName = findViewById(R.id.userName)
+        email = findViewById(R.id.email)
+        val uName = intent.getStringExtra("uname")
+        val uEmail = intent.getStringExtra("uemail")
+        if (uName == null){
+            userName.text =  "Name : Name will be shown here"
+            email.text = "Email : Email will be shown here"
+        }else{
+            userName.text =  "Name : $uName"
+            email.text = "Email : $uEmail"
+        }
+
         naviBtn.setOnClickListener{
             startActivity(Intent(this, SecondActivity ::class.java))
         }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -8,30 +9,33 @@ import android.widget.TextView
 import kotlin.math.log
 
 class SecondActivity : AppCompatActivity() {
-    private lateinit var email : TextView
+    private lateinit var emailText : TextView
     private lateinit var nameText : TextView
     private lateinit var onSave : Button
-    private lateinit var emailAddress : EditText
+    private lateinit var email : EditText
     private lateinit var name : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        email = findViewById(R.id.secondText)
+        emailText = findViewById(R.id.emailText)
         onSave = findViewById(R.id.saveBtn)
         nameText = findViewById(R.id.nameText)
-        emailAddress = findViewById(R.id.emailAddress)
-        email.text = ""
+        email = findViewById(R.id.email)
+        emailText.text = ""
         name = findViewById(R.id.name)
         nameText.text = ""
 
 
         onSave.setOnClickListener {
-            if(emailAddress.text.isEmpty() ) {
-                email.text = "Text Field cant be empty"
+            if(email.text.isEmpty() ) {
+                emailText.text = "Text Field cant be empty"
             } else {
-                email.text = emailAddress.text
+                emailText.text = email.text
                 nameText.text = name.text
+                    startActivity(Intent(this, MainActivity ::class.java).putExtra("uname", name.text.toString()).putExtra("uemail", email.text.toString())
+
+                )
             }
         }
 
