9c0e2640dabbcbe660f1f2f53d8bc9720b9e5d74
Ali Hassan Cheema <ahcheema12@gmail.com>
2023-01-25 11:08:39 +0500

    activity lifecycle logs added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
      private lateinit var userName: TextView
      private lateinit var email: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("TAG", "onCreate: Activity1 Created")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         naviBtn = findViewById(R.id.navigationBtn)
@@ -28,11 +30,49 @@ class MainActivity : AppCompatActivity() {
 
         naviBtn.setOnClickListener{
             val intent = Intent(this, SecondActivity ::class.java)
+
             intent.putExtra("name", "name")
             intent.putExtra("email", "name")
-            startActivityForResult(intent, 1)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+//            startActivityForResult(intent, 1)
 
         }
+    }
+
+    override fun onStart() {
+        Log.e("TAG", "onStart: Activity1 Started", )
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.e("TAG", "onResume: Activity1 Resumed", )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.e("TAG", "onPause: Activity1 paused", )
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.e("TAG", "onStop: Activity1 Stopped", )
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.e("TAG", "onRestart: Activity1 Restarted", )
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.e("TAG", "onDestroy: Activity1 Destroyed", )
+        super.onDestroy()
+    }
+
+    override fun startActivity(intent: Intent?) {
+
+        super.startActivity(intent)
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -40,6 +80,10 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1){
              userName.text = "Name: ${data!!.getStringExtra("name")}"
             email.text = "Email: ${data!!.getStringExtra("email")}"
+        }else{
+            Log.e("TAG", "onActivityResult: Null Value returned", )
         }
     }
+
+
 }
--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -3,9 +3,11 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.activity.OnBackPressedCallback
 import kotlin.math.log
 
 class SecondActivity : AppCompatActivity() {
@@ -16,6 +18,7 @@ class SecondActivity : AppCompatActivity() {
     private lateinit var name : EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.e("TAG", "onCreate: Activity2 Created", )
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
         emailText = findViewById(R.id.emailText)
@@ -28,6 +31,7 @@ class SecondActivity : AppCompatActivity() {
 
 
         onSave.setOnClickListener {
+
             if(email.text.isEmpty() ) {
                 emailText.text = "Text Field cant be empty"
             } else {
@@ -42,5 +46,49 @@ class SecondActivity : AppCompatActivity() {
             }
         }
 
+        onBackPressedDispatcher.addCallback(this /* lifecycle owner */, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+//                val intent = Intent()
+//                intent.putExtra("name", "${name.text}")
+//                intent.putExtra("email", "${email.text}")
+//                setResult(RESULT_OK, intent)
+                finish()
+                Log.e("TAG", "handleOnBackPressed: Back Pressed", )
+               // Back is pressed... Finishing the activity
+            }
+        })
+
     }
+    override fun onStart() {
+        Log.e("TAG", "onStart: Activity2 Started", )
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.e("TAG", "onResume: Activity2 Resumed", )
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.e("TAG", "onPause: Activity2 paused", )
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.e("TAG", "onStop: Activity2 Stopped", )
+        super.onStop()
+    }
+
+    override fun onRestart() {
+        Log.e("TAG", "onRestart: Activity2 Restarted", )
+        super.onRestart()
+    }
+
+    override fun onDestroy() {
+        Log.e("TAG", "onDestroy: Activity2 Destroyed", )
+        super.onDestroy()
+    }
+
+
+
 }
